ae5f000e6a48a31826b83347e30945915840dd4a
dan@retrobadger.net <dan@retrobadger.net>
2014-09-17 14:31:34 +0100

    Some tweaks to get the video quality actually working properly.  Also added ability to change resolution

--- README.md
+++ README.md
@@ -12,10 +12,16 @@
 
 ## Usage
 Ensure the script has the executable bit set then simply run it using a command such as
-```./videoToHTML5.sh original_video.mov 25 5```
+```./videoToHTML5.sh original_video.mov 70 5 640x360```
 * Param1 = the video file
 * Param2 = The video quality to use
 * Param3 = The audio quality to use
 
+If you want a more high-res video for desktop use (the above could look grainy when full-screen) then try these settings
+```./videoToHTML5.sh original_video.mov 80 5 1280x720```
+
 ## Troubleshooting
 *No known bugs as yet*
+
+## Notes
+You may already know that with video compression, fine tuning is often required.  This script may then be just a good starting point for you to jump in and tweak a few of the settings yourself to get exactly the filesize and quality you desire.
--- videoToHTML5.sh
+++ videoToHTML5.sh
@@ -4,22 +4,30 @@
 IN=$1
 VIDEOQUALITY=$2
 AUDIOQUALITY=$3
+RESOLUTION=$4
 OUT=$(echo $1 | sed 's/^\(.*\)\.[a-zA-Z0-9]*$/\1/')
+
+# Video quality values
+QMAX=$(echo "63-($VIDEOQUALITY/1.7)" | bc)
+QMIN=$(echo "$QMAX - 1" | bc)
+QSCALE=$(echo "31-($VIDEOQUALITY/3.3)" | bc)
 
 # Print out variables received
 echo "Creating videos using IN=$IN OUT=$OUT VIDEOQUALITY=$VIDEOQUALITY AUDIOQUALITY=$AUDIOQUALITY"
+echo "QMAX = $QMAX"
+echo "QMIN = $QMIN"
+echo "QSCALE = $QSCALE"
 
 # Hardcoded Values
-RESOLUTION='640x360'
 THREADS='2'
-VIDEOCONSTANTRATEFACTOR='22' #this is only for x254 video encoding
+VIDEOCONSTANTRATEFACTOR=$(echo "51-($VIDEOQUALITY/2)" | bc) #this is only for x254 video encoding
 AUDIOCHANNELS='2'
 
 # webm
-ffmpeg -i $IN -acodec libvorbis -aq $AUDIOQUALITY -ac $AUDIOCHANNELS -qmax $VIDEOQUALITY -qscale $VIDEOQUALITY -threads $THREADS -s $RESOLUTION $OUT.webm
+ffmpeg -i $IN -acodec libvorbis -vcodec libvpx -b 1M -aq $AUDIOQUALITY -ac $AUDIOCHANNELS -qmin $QMIN -qmax $QMAX -qscale $QSCALE -threads $THREADS -s $RESOLUTION $OUT.webm
 
 # mp4
-ffmpeg -i $IN -acodec aac -strict experimental -vcodec libx264 -crf $VIDEOCONSTANTRATEFACTOR -aq $AUDIOQUALITY -ac $AUDIOCHANNELS -qmax $VIDEOQUALITY -qscale $VIDEOQUALITY -threads $THREADS -s $RESOLUTION $OUT.mp4
+ffmpeg -i $IN -acodec aac -strict experimental -vcodec libx264 -crf $VIDEOCONSTANTRATEFACTOR -aq $AUDIOQUALITY -ac $AUDIOCHANNELS -qmin $QMIN -qmax $QMAX -qscale $QSCALE -threads $THREADS -s $RESOLUTION $OUT.mp4
 
 # ogg (if you want to support older Firefox)
-ffmpeg -i $IN -acodec libvorbis -vcodec libtheora -aq $AUDIOQUALITY -ac $AUDIOCHANNELS -qmax $VIDEOQUALITY -qscale $VIDEOQUALITY -threads $THREADS -s $RESOLUTION $OUT.ogv
+ffmpeg -i $IN -acodec libvorbis -vcodec libtheora -aq $AUDIOQUALITY -ac $AUDIOCHANNELS -qmin $QMIN -qmax $QMAX -qscale $QSCALE -threads $THREADS -s $RESOLUTION $OUT.ogv
